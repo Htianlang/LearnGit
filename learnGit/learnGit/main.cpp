a2044c82fb07eeadce22a3888081029862e78b6f
huang tianlang <htl246810@126.com>
2020-06-04 16:31:39 +0800

    First Commit

---
+++ learnGit/learnGit/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  learnGit
+//
+//  Created by 天朗 on 2020/6/4.
+//  Copyright © 2020 天朗. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
